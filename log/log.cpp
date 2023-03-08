75dcbdb7c6764c000bd357a889feb9d60abbc053
AClolinta <AClolinta@gmail.com>
2023-03-08 03:06:27 +0000

    学习一个时间戳

--- log/log.cpp
+++ log/log.cpp
@@ -1,18 +1,33 @@
+/*
+ * @Author: AClolinta AClolinta@gmail.com
+ * @Date: 2023-03-07 12:18:55
+ * @LastEditors: AClolinta AClolinta@gmail.com
+ * @LastEditTime: 2023-03-08 03:02:57
+ * @FilePath: /TinyWebServer/log/log.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
+ * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include "log.hpp"
 
 #include <error.h>
 #include <stdarg.h>
-#include <time.h>
 
+#include <chrono>
+#include <ctime>
+#include <locale>
 #include <stdexcept>
+#include <vector>
 
 #define THROW_ERROR(error) throw std::logic_error((#error))
 
 namespace aclolinta {
+namespace logger {
 
 Logger *Logger::m_instance = nullptr;
 
-Logger::Logger() : m_max(0), m_len(0), m_level(1 /*debug*/){};
+Logger::Logger()
+    : m_max(0), m_len(0), m_level(LoggerLevel::DEBUG /*debug级别*/){};
+Logger::~Logger() { m_fout.close(); }
 
 void Logger::Close() {
     if (m_instance != nullptr) {
@@ -40,4 +55,23 @@ void Logger::Open(std::string_view filename) {
 
     m_len = m_fout.tellp();  // 获取 当前 put 流指针的位置
 }
+
+void Logger::Log(LoggerLevel level, std::string_view file, size_t line,
+                 std::string_view format, ...) {
+    if (m_level > level) {
+        return;  // 等级太低滚蛋
+    }
+    if (m_fout.fail()) {
+        throw std::logic_error("OPEN LOG FILE IN LOGGER FAILED: " + m_filename);
+    }
+    // 时间输出
+    std::locale::global(std::locale("zh_CN.utf8"));
+    time_t ticks = std::chrono::system_clock::to_time_t(
+        std::chrono::system_clock::now());  // 先获取时间再转化成time_t格式
+    char timestamp[32];
+    std::fill(timestamp[0],timestamp[31],'0');
+    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", std::localtime(&ticks));
+}
+
+}  // namespace logger
 }  // namespace aclolinta
--- log/log.hpp
+++ log/log.hpp
@@ -1,16 +1,9 @@
+
 /*
  * @Author: AClolinta AClolinta@gmail.com
  * @Date: 2023-03-07 11:58:01
  * @LastEditors: AClolinta AClolinta@gmail.com
- * @LastEditTime: 2023-03-07 12:24:57
- * @FilePath: /TinyWebServer/log/log.hpp
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-/*
- * @Author: AClolinta AClolinta@gmail.com
- * @Date: 2023-03-07 11:58:01
- * @LastEditors: AClolinta AClolinta@gmail.com
- * @LastEditTime: 2023-03-07 12:18:36
+ * @LastEditTime: 2023-03-08 02:37:53
  * @FilePath: /TinyWebServer/log/log.hpp
  * @Description: 日志器
  */
@@ -21,8 +14,9 @@
 #include <string_view>
 
 namespace aclolinta {
+namespace logger {
 
-enum class LoggerLevel :int {
+enum class LoggerLevel : int {
     UNKNOW = 0,
     DEBUG = 1,
     INFO = 2,
@@ -38,21 +32,24 @@ class Logger {
     void Rotate();
     /* data */
    private:
-    std::string m_filename;
-    std::ofstream m_fout;
+    std::string m_filename;  // 文件名
+    std::ofstream m_fout;    // 输出文件流
     int m_max;
-    int m_len;
-    int m_level;
+    int m_len;  // m_foutput 流中指针的位置
+    LoggerLevel m_level;
     static std::string s_level;  // 六个等级
     static Logger* m_instance;
 
    public:
-   static Logger* Instance();//单例
-   void open(std::string_view filename);//打开文件
-   void close();
-   void Log( LoggerLevel level, std::string_view file, size_t line , std::string_view format, ...);
-   void Max(int bytes);
-   void Level(int level);
+    static Logger* GetInstance();  // 单例
+    static void Close();
+    void Open(std::string_view filename);  // 打开文件
+
+    void Log(LoggerLevel level, std::string_view file, size_t line,
+             std::string_view format, ...);
+    void Max(int bytes);
+    void Level(int level);
 };
+}  // namespace logger
 
 }  // namespace aclolinta
