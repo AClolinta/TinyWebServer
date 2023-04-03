9b349677bcd0e072bb54e05a60f562408f821bd4
AClolinta <AClolinta@gmail.com>
2023-04-03 12:50:12 +0000

    核心部分

---
+++ socket/SocketHandler.hpp
@@ -0,0 +1,50 @@
+/*
+ * @Author: AClolinta AClolinta@gmail.com
+ * @Date: 2023-04-03 12:05:40
+ * @LastEditors: AClolinta AClolinta@gmail.com
+ * @LastEditTime: 2023-04-03 12:49:21
+ * @FilePath: /TinyWebServer/socket/SocketHandler.hpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
+ * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#pragma once
+
+#include <list>
+
+#include <string>
+#include<string_view> 
+
+#include "../utility/ObjectPool.hpp"
+#include "EventPoller.hpp"
+#include "Socket.hpp"
+
+using namespace aclolinta::utility;
+
+#include "../thread/Mutex.hpp"
+#include "../thread/Task.hpp"
+
+namespace aclolinta {
+namespace socket {
+class SocketHandler {
+   private:
+    /* data */
+    EventPoller* m_epoll;
+    Socket* m_server;
+    ObjectPool<Socket> m_sockt_pool;
+    Mutex m_mutex;
+
+   public:
+    SocketHandler(/* args */);
+    ~SocketHandler();
+
+   public:
+
+    void Listen(std::string_view ip, size_t port);
+    void Attach(Socket * socket);
+    void Detach(Socket* socket);
+    void Remove(Socket* socket);
+    void Handle(size_t max_connect, size_t wait_time);
+};
+
+}  // namespace socket
+}  // namespace aclolinta
