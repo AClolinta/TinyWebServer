0a9d289a680f61db9459175a30a19d6ffd119925
AClolinta <AClolinta@gmail.com>
2023-04-04 13:10:47 +0000

    基于事件轮询模型的 Socket 服务器

--- socket/SocketHandler.cpp
+++ socket/SocketHandler.cpp
@@ -2,7 +2,7 @@
  * @Author: AClolinta AClolinta@gmail.com
  * @Date: 2023-04-03 13:09:31
  * @LastEditors: AClolinta AClolinta@gmail.com
- * @LastEditTime: 2023-04-03 13:16:41
+ * @LastEditTime: 2023-04-04 09:25:09
  * @FilePath: /TinyWebServer/socket/SocketHandler.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
  * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
@@ -17,8 +17,20 @@ using namespace aclolinta::utility;
 using namespace aclolinta::logger;
 
 #include "../thread/AutoLock.hpp"
-#include "../thread/Task.hpp"
+#include "../task/Task.hpp"
+using namespace aclolinta::task;
+
 #include "../thread/TaskDispatcher.hpp"
 #include "ServerSocket.hpp"
 using namespace aclolinta::socket;
 
+SocketHandler::~SocketHandler(){
+    if(nullptr != m_epoll){
+        delete m_epoll;
+        m_epoll = nullptr;
+    }
+
+    if (nullptr != m_server) {
+        delete m_server;
+        m_server = nullptr;
+    }
